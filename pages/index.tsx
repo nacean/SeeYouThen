85e47b5589d77d4826cc3508e353c558ae3e29a4
nacean <nacean@naver.com>
2022-05-20 19:55:21 +0900

    /home 내용 변경, timeBlock 색상 hover 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import type { NextPage } from 'next';
-import DatePickContainer from '../components/home/datePick/DatePickContainer';
-import TimeResultContainer from '../components/home/timeResult/TimeResultContainer';
 import styles from './Home.module.scss';
 
 const Home: NextPage = () => {
   return (
     <article className={styles.homeContainer}>
-      <DatePickContainer />
-      <TimeResultContainer />
+      <h2 className={styles.serviceName}>See You Then</h2>
+      <p className={styles.serviceDescription}>
+        When we meet ? Ok, see you then!!
+      </p>
     </article>
   );
 };
