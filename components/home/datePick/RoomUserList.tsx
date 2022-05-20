2d585c661b5847088bdd968e18127af48fddd1cc
nacean <nacean@naver.com>
2022-05-20 18:06:32 +0900

    선택된 roomUser 제거 기능 구현, 제거 시 체크했던 block 도 비활성화로 변경

--- components/home/datePick/RoomUserList.tsx
+++ components/home/datePick/RoomUserList.tsx
@@ -1,20 +1,62 @@
 import React from 'react';
 import styles from './RoomUserList.module.scss';
-import { useRecoilValue } from 'recoil';
+import { useRecoilState } from 'recoil';
 import roomUsersState from '../../../atoms/roomUserAtoms/roomUsersState';
 import RoomUser from './RoomUser';
 import nowPickUserState from '../../../atoms/roomUserAtoms/nowPickUserState';
+import { Button, Popconfirm } from 'antd';
+import timeBlockState, {
+  blockType,
+  timeBlockType,
+} from '../../../atoms/timeAtoms/timeBlockState';
 
 function RoomUserList() {
-  const roomUsers = useRecoilValue(roomUsersState);
-  const nowPickUser = useRecoilValue(nowPickUserState);
+  const [roomUsers, setRoomUsers] = useRecoilState(roomUsersState);
+  const [nowPickUser, setNowPickUser] = useRecoilState(nowPickUserState);
+  const [timeBlocks, setTimeBlocks] = useRecoilState(timeBlockState);
+
+  const onDeleteNameConfirm = () => {
+    const filteredRoomUsers: string[] = roomUsers.filter(
+      (roomUserParam: string) => nowPickUser !== roomUserParam,
+    );
+
+    const newTimeBlocks: timeBlockType[] = timeBlocks.map(
+      (timeBlockParam: timeBlockType) => ({
+        ...timeBlockParam,
+        blocks: timeBlockParam.blocks.map((blockParam: blockType) => ({
+          ...blockParam,
+          usingUsers: blockParam.usingUsers.filter(
+            (userParam) => userParam !== nowPickUser,
+          ),
+        })),
+      }),
+    );
+
+    setRoomUsers(filteredRoomUsers);
+    setTimeBlocks(newTimeBlocks);
+    setNowPickUser(null);
+  };
+
   return (
     <div className={styles.roomUserListContainer}>
-      {nowPickUser ? (
-        <h3>현재 선택한 이름 : {nowPickUser}</h3>
-      ) : (
-        <h3>현재 선택된 이름이 없습니다</h3>
-      )}
+      <div className={styles.pickedNameAndDelete}>
+        {nowPickUser ? (
+          <h3>
+            현재 선택한 이름 : <h4>{nowPickUser}</h4>
+          </h3>
+        ) : (
+          <h3>현재 선택된 이름이 없습니다</h3>
+        )}
+        <Popconfirm
+          title="선택된 인원의 이름이 제거됩니다. 하시겠습니까?"
+          onConfirm={onDeleteNameConfirm}
+          okText="제거"
+          cancelText="취소"
+          placement="bottomLeft"
+        >
+          <Button danger>제거</Button>
+        </Popconfirm>
+      </div>
       <ul className={styles.roomUserList}>
         {roomUsers.map((roomUser: string) => (
           <RoomUser username={roomUser} isPicked={roomUser === nowPickUser} />
