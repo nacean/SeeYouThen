fb4bca8df147888073d539b2f22216a8f4666d54
nacean <nacean@naver.com>
2022-05-20 18:39:52 +0900

    makeRoomPage 에서 Form 을 component로 분리

---
+++ components/makeRoom/MakeRoomForm.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Button, Form, Input } from 'antd';
+import { v4 as uuidv4 } from 'uuid';
+import { useRouter } from 'next/router';
+
+interface submitType {
+  roomName: string;
+}
+
+function MakeRoomForm() {
+  const roomRouter = useRouter();
+  const onFinish = (submitParam: submitType) => {
+    const uuid = uuidv4();
+    const { roomName } = submitParam;
+    roomRouter.push(`/roomNo/${uuid}?roomName=${roomName}`);
+  };
+  return (
+    <Form layout="inline" onFinish={onFinish}>
+      <Form.Item
+        label="roomName"
+        name="roomName"
+        rules={[{ required: true, message: '방이름을 입력하세요!' }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          방만들기
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+}
+
+export default MakeRoomForm;
--- pages/MakeRoomPage.tsx
+++ pages/MakeRoomPage.tsx
@@ -1,38 +1,12 @@
-import { Button, Form, Input } from 'antd';
 import React from 'react';
 import styles from './MakeRoomPage.module.scss';
-import { v4 as uuidv4 } from 'uuid';
-import { useRouter } from 'next/router';
-
-interface submitType {
-  roomName: string;
-}
+import MakeRoomForm from '../components/makeRoom/MakeRoomForm';
 
 function MakeRoomPage() {
-  const roomRouter = useRouter();
-  const onFinish = (submitParam: submitType) => {
-    const uuid = uuidv4();
-    const { roomName } = submitParam;
-    roomRouter.push(`/roomNo/${uuid}?roomName=${roomName}`);
-  };
   return (
     <article className={styles.makeRoomPageContainer}>
       <h2>방 만들기</h2>
-      <Form layout="inline" onFinish={onFinish}>
-        <Form.Item
-          label="roomName"
-          name="roomName"
-          rules={[{ required: true, message: '방이름을 입력하세요!' }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            방만들기
-          </Button>
-        </Form.Item>
-      </Form>
+      <MakeRoomForm />
     </article>
   );
 }
