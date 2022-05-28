aed21d5b17edec743aa8591f85f34b322b97b869
nacean <nacean@naver.com>
2022-05-28 18:29:07 +0900

    :art: url copy에 도메인 주소 추가, 버튼 클릭시 완전한 url로 copy하도록 구현

--- components/home/roomHead/RoomTitleAndCopy.tsx
+++ components/home/roomHead/RoomTitleAndCopy.tsx
@@ -10,11 +10,12 @@ const { Paragraph } = Typography;
 function RoomTitleAndCopy() {
   const roomName = useRecoilValue(roomNameState);
   const roomId = useRecoilValue(roomIdState);
+  const roomUrl = `https://see-you-then.vercel.app/roomNo/${roomId}?roomName=${roomName}`;
   return (
     <div className={styles.roomHeadContainer}>
       <h2>{roomName}</h2>
       <div className={styles.copyUrl}>
-        <Paragraph copyable={{ text: roomId as string }}>
+        <Paragraph copyable={{ text: roomUrl }}>
           주소를 복사하고 공유하세요
         </Paragraph>
       </div>
