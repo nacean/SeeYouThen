4b642a166fef6e1acc06928c957716fe2c89d1d4
nacean <nacean@naver.com>
2022-05-30 23:17:58 +0900

    :sparkles: 존재하는 방에 대해 uuid와 이름이 일치하지 않을 경우 home 이동하게 구현

---
+++ modules/dbModules/checkRoomNameId.ts
@@ -0,0 +1,18 @@
+import { doc, getDoc } from 'firebase/firestore/lite';
+import { db } from '../../fireStore/fireStoreApp';
+
+async function checkRoomNameId(roomNameParam: string, roomIdParam: string) {
+  const roomRef = doc(db, 'roomDB', roomIdParam);
+  const roomSnapShot = await getDoc(roomRef);
+
+  if (roomSnapShot.exists()) {
+    const { roomName } = roomSnapShot.data();
+    if (roomName === roomNameParam) return true;
+
+    return false;
+  } else {
+    return true;
+  }
+}
+
+export default checkRoomNameId;
