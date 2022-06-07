0511fcb94fad8852f75f576718f73ab9b5282594
nacean <nacean@naver.com>
2022-06-07 17:58:44 +0900

    :lipstick: mobile : timeBlock 부분 scroll 하게끔 반응형 구현

--- components/home/timeResult/TimeResultContainer.tsx
+++ components/home/timeResult/TimeResultContainer.tsx
@@ -1,4 +1,3 @@
-import moment from 'moment';
 import React from 'react';
 import { useRecoilValue } from 'recoil';
 import roomIdState from '../../../atoms/roomInfo/roomIdState';
@@ -9,6 +8,7 @@ import timeBlockState, {
   timeBlockType,
 } from '../../../atoms/timeAtoms/timeBlockState';
 import SwitchAllSelect from './SwitchAllSelect';
+import TimeLine from './TimeLine';
 import TimeResultBlock from './TimeResultBlock';
 import styles from './TimeResultContainer.module.scss';
 
@@ -18,55 +18,37 @@ function TimeResultContainer() {
   const roomUsers = useRecoilValue(roomUsersState);
   const roomId = useRecoilValue(roomIdState);
 
-  const makeTimeLine = () => {
-    const timeLines: string[] = [];
-    timeLines.push('time');
-
-    const TIME_MAX_LENGTH = 48;
-    for (let i = 0; i <= TIME_MAX_LENGTH; i++) {
-      timeLines.push(
-        moment('00:00', 'HH:mm')
-          .add(i * 30, 'minutes')
-          .format('HH:mm'),
-      );
-    }
-
-    let timeLineKeyNum = 0;
-
-    return timeLines.map((timeLineParam: string) => (
-      <li key={timeLineKeyNum++} className={styles.timeLineList}>
-        {timeLineParam}
-      </li>
-    ));
-  };
-
   return (
     <section className={styles.timeResultContainer}>
       <SwitchAllSelect />
-      <div className={styles.timePerDateBox}>
-        <ul>{makeTimeLine()}</ul>
-        {timeBlocks.map((timeBlockParam: timeBlockType) => {
-          return (
-            <div key={timeBlockParam.date} className={styles.timeBlock}>
-              <h3>{timeBlockParam.date}</h3>
-              {timeBlockParam.blocks.map((blockParam: blockType) => {
-                return (
-                  <TimeResultBlock
-                    key={`${blockParam.row}${timeBlockParam.col}`}
-                    row={blockParam.row}
-                    col={timeBlockParam.col}
-                    colored={blockParam.usingUsers.indexOf(nowPickUser) !== -1}
-                    allUserSelect={
-                      roomUsers.length !== 0 &&
-                      roomUsers.length === blockParam.usingUsers.length
-                    }
-                    blockUsingUsers={blockParam.usingUsers}
-                  />
-                );
-              })}
-            </div>
-          );
-        })}
+      <div className={styles.timeLineAndBlocks}>
+        <TimeLine />
+        <div className={styles.alltimeBlocks}>
+          {timeBlocks.map((timeBlockParam: timeBlockType) => {
+            return (
+              <div key={timeBlockParam.date} className={styles.timeBlock}>
+                <h3>{timeBlockParam.date}</h3>
+                {timeBlockParam.blocks.map((blockParam: blockType) => {
+                  return (
+                    <TimeResultBlock
+                      key={`${blockParam.row}${timeBlockParam.col}`}
+                      row={blockParam.row}
+                      col={timeBlockParam.col}
+                      colored={
+                        blockParam.usingUsers.indexOf(nowPickUser) !== -1
+                      }
+                      allUserSelect={
+                        roomUsers.length !== 0 &&
+                        roomUsers.length === blockParam.usingUsers.length
+                      }
+                      blockUsingUsers={blockParam.usingUsers}
+                    />
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
