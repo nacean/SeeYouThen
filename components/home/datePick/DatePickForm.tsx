2ec05d3d8134d7fb91cae9c87f2df251993200ac
nacean <nacean@naver.com>
2022-06-11 22:03:00 +0900

    :construction: time도 고르게 구현중: timePicker 추가, value=null시 alert

--- components/home/datePick/DatePickForm.tsx
+++ components/home/datePick/DatePickForm.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, DatePicker, Popconfirm } from 'antd';
+import { Button, DatePicker, Popconfirm, TimePicker } from 'antd';
 import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import datePickState from '../../../atoms/timeAtoms/datePickState';
 import { Moment } from 'moment';
@@ -12,30 +12,40 @@ import addRoomInfo from '../../../modules/dbModules/addRoomInfo';
 import roomUsersState from '../../../atoms/roomUserAtoms/roomUsersState';
 import roomNameState from '../../../atoms/roomInfo/roomNameState';
 import styles from './DatePickForm.module.scss';
+import nowPickTimeState from '../../../atoms/timeAtoms/nowPickTimeState';
 const { RangePicker } = DatePicker;
 
 function DatePickForm() {
   const [pickedDates, setPickedDates] =
     useRecoilState<(Moment | null)[]>(datePickState);
+  const [pickedTimes, setPickedTimes] =
+    useRecoilState<(Moment | null)[]>(nowPickTimeState);
   const setTimeBlocks = useSetRecoilState(timeBlockState);
   const roomId = useRecoilValue(roomIdState);
   const roomName = useRecoilValue(roomNameState);
   const roomUsers = useRecoilValue(roomUsersState);
 
-  const onDatePickChange = (timeParam: (Moment | null)[] | null) => {
-    setPickedDates(timeParam);
+  const onDatePickChange = (dateParam: (Moment | null)[] | null) => {
+    setPickedDates(dateParam);
+  };
+
+  const onTimePickChange = (timeParam: (Moment | null)[] | null) => {
+    setPickedTimes(timeParam);
   };
 
   const onDataChangeConfirm = () => {
-    if (pickedDates === null) {
-      setTimeBlocks([]);
-      setPickedDates([null, null]);
+    if (pickedDates === null || pickedTimes === null) {
+      alert('날짜와 시간 설정이 필요합니다.');
       return;
     }
 
-    if (pickedDates[0] === null || pickedDates[1] === null) {
-      setTimeBlocks([]);
-      setPickedDates([null, null]);
+    if (
+      pickedDates[0] === null ||
+      pickedDates[1] === null ||
+      pickedTimes[0] === null ||
+      pickedTimes[1] === null
+    ) {
+      alert('날짜와 시간 설정이 필요합니다.');
       return;
     }
 
@@ -75,13 +85,20 @@ function DatePickForm() {
         onCalendarChange={onDatePickChange}
         placement="topRight"
       />
+      <TimePicker.RangePicker
+        value={pickedTimes ? [pickedTimes[0], pickedTimes[1]] : null}
+        minuteStep={30}
+        format="HH:mm"
+        placement="bottomRight"
+        onChange={onTimePickChange}
+      />
       <Popconfirm
         title="날짜변경 시 데이터가 초기화 됩니다. 하시겠습니까?"
         onConfirm={onDataChangeConfirm}
         okText="변경"
         cancelText="취소"
       >
-        <Button type="primary">날짜변경</Button>
+        <Button type="primary">변경</Button>
       </Popconfirm>
     </div>
   );
