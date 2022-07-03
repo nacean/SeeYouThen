2b1492e45398f47673f62ebbb7169226ec8303fd
nacean <nacean@naver.com>
2022-07-03 16:52:37 +0900

    :bug: time format 형식 차이로 인한 block 미생성 bug fix

--- modules/timeModules/getBlankTimeBlock.ts
+++ modules/timeModules/getBlankTimeBlock.ts
@@ -5,7 +5,7 @@ function getBlankTimeBlock(startTime: Moment, endTime: Moment): blockType[] {
   const timeArray: blockType[] = [];
   let rowNum = 0;
 
-  while (endTime.format('hh:mm') !== startTime.format('hh:mm')) {
+  while (endTime.format('HH:mm') !== startTime.format('HH:mm')) {
     timeArray.push({ row: rowNum++, usingUsers: [] });
     startTime.add(30, 'm');
   }
